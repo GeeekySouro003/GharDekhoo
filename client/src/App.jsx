c0f727e19703ef162b46148b2caa8a7ec44afd49
Souradip <dgsouro77@gmail.com>
2024-03-07 13:42:05 +0530

    Added Website Router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,26 @@
-import Header from "./Components/Header/Header.jsx";
-import Hero from "./Components/Hero/Hero.jsx";
-import './App.css'
-import Companies from "./Components/Companies/Companies.jsx";
-import Properties from "./Components/Properties/Properties.jsx";
-import Values from "./Components/Values/Values.jsx";
-import Contact from "./Components/Contact/Contact.jsx";
-import GetStarted from "./Components/GetStarted/GetStarted.jsx";
-import Footer from "./Components/Footer/Footer.jsx";
+import { Suspense } from 'react';
+import './App.css';
+import Website from "./Pages/Website.jsx";
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import { LayoutGroup } from 'framer-motion';
 function App()
 {
   return(
-    <div className="App">
-      <div>
-        <div className="white-gradient"/>
-      <Header />
-      <Hero/> 
-      </div>
-      <Companies/>
-       <Properties/>
-       <Values/>
-      <Contact/>
-      <GetStarted/>
-     <Footer/>   
-    </div>
+    <BrowserRouter>
+    <Suspense fallback={<div>Loading...</div>}>
+    <Routes>
+      <Route element={<Layout />} >
+      <Route path='/' element={<Website/>} />
+      </Route>
+      
+    
+    </Routes>
+    </Suspense>
+    
+   
+     
+    </BrowserRouter>
+   
   ); 
   
 }
---
+++ client/src/Pages/Website.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Companies from "../Components/Companies/Companies.jsx";
+import Properties from "../Components/Properties/Properties.jsx";
+import Values from "../Components/Values/Values.jsx";
+import Contact from "../Components/Contact/Contact.jsx";
+import GetStarted from "../Components/GetStarted/GetStarted.jsx";
+import Footer from "../Components/Footer/Footer.jsx";
+import Header from "../Components/Header/Header.jsx";
+import Hero from "../Components/Hero/Hero.jsx";
+
+const Website = () => {
+  return (
+    <div className="App">
+      <div>
+        <div className="white-gradient"/>
+      <Header />
+      <Hero/> 
+      </div>
+      <Companies/>
+       <Properties/>
+       <Values/>
+      <Contact/>
+      <GetStarted/>
+     <Footer/>   
+    </div>
+  )
+}
+
+export default Website
